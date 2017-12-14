7d4bd4fab6da4e3ed8cb60ae7457233f2c639ba4
Pattawut Manapongpun <pmanapon@ucsc.edu>
2017-12-14 13:27:30 +0700

    combile codes update

--- Bill.py
+++ Bill.py
@@ -1,19 +1,20 @@
-#pseudo code
+#Final Project
 #Edited by Pattawut Manapongpun (Bill)
 
-
-#getCard function - just use for checking my part
+#####################################################################################################
+# Main deck
+#getCard function
 from random import randrange
 class Deck:
     def __init__(self):
         #♢♡♠♣♥♦
         Card_Val = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-        Card_Sym = ['♠ Spades', '♥ Hearts', '♣ Clubs', '♦ Diamonds']
+        Card_Sym = ['♠Spades', '♥Hearts', '♣Clubs', '♦Diamonds']
         self.cards = []
         #Create a deck of cards
         for i in Card_Val:
             for j in Card_Sym:
-                self.cards.append(i + '' + j)
+                self.cards.append(i + ' ' + j)
 
     def getCard(self,deck):
         '''Return a random card and delete it from deck'''
@@ -31,7 +32,7 @@ class Deck:
             return 10
         else:
             return int(card[0])
-
+#######################################################################
 
 # newdeck = Deck()
 # print(newdeck.cards)
@@ -44,7 +45,7 @@ class Deck:
 def getValue(card):
     '''Return the value of card'''
     if card[0] == 'A':
-        return 1 #or 11
+        return 1 #return 1 for now but will add 10 later if needed
     elif card[0]=='J' or card[0]=='Q' or card[0]=='K':
         return 10
     elif card[0]=='1':
@@ -71,11 +72,14 @@ def enter_bet():
     print("Good luck!")
     return bet
 
+#############################################################################
 def delay():
     import time
     print("loding....")
     time.sleep(1)
 
+
+############################################################################
 def split(PlayCard1,PlayCard2):
     print("Splitting...")
     global newdeck
@@ -83,7 +87,18 @@ def split(PlayCard1,PlayCard2):
     PlayCard2_1 = newdeck.getCard(newdeck.cards)
     print("Hand1: " + PlayCard1 + " and " + PlayCard1_1 +" || Hand2: " + PlayCard2 + " and " + PlayCard2_1 )
 
-#def hit():
+def hit():
+    global newdeck
+    global PlayCard
+    global PlaySum
+    hitcard = newdeck.getCard(newdeck.cards)
+    PlayCard.append(hitcard)  # call hit function
+    print("Your cards are " + PlayCard[0] , end="")
+    for i in range(1,len(PlayCard)):
+        print(" || "+ PlayCard[i], end="")
+    print("")
+    PlaySum += getValue(hitcard)
+
 
 #def double():
 
@@ -91,22 +106,26 @@ def split(PlayCard1,PlayCard2):
 
 
 def startRound():
-
+    """Play one round and return win or lose"""
     global newdeck
     newdeck = Deck()
+    global PlayCard
+    PlayCard = [newdeck.getCard(newdeck.cards),newdeck.getCard(newdeck.cards)]
     DealerCard1 = newdeck.getCard(newdeck.cards)
-    PlayCard1 = newdeck.getCard(newdeck.cards)
+    # PlayCard1 = newdeck.getCard(newdeck.cards)
     DealerCard2 = newdeck.getCard(newdeck.cards)
-    PlayCard2 = newdeck.getCard(newdeck.cards)
-
+    # PlayCard2 = newdeck.getCard(newdeck.cards)
+    DelerSum = 0
+    global PlaySum
+    PlaySum = getValue(PlayCard[0])+getValue(PlayCard[1])
     # delay()
     print("One of Dealer's card is " + DealerCard1)
     # delay()
-    print("Your cards are " + PlayCard1 + " and " + PlayCard2)
+    print("Your cards are " + PlayCard[0] + " || " + PlayCard[1])
 
 
 
-    if PlayCard1[0] == PlayCard2[0]:
+    if PlayCard[0][0] == PlayCard[1][0]:
         print("Do you want to SPLIT hands? (Y/N)")
         if input().upper() == 'Y':
             split(PlayCard1,PlayCard2)    #call split function
@@ -116,19 +135,41 @@ def startRound():
     print("Do you want to HIT or STAND or DOUBLE? (H/S/D)")
     res = input().upper()
     if res == 'H':
-        PlayCard3 = newdeck.getCard(newdeck.cards) #call hit function
-        print("Your cards are " + PlayCard1 + " and " + PlayCard2 + " and " + PlayCard3)
+        PlayCard.append(newdeck.getCard(newdeck.cards)) #call hit function
+        PlaySum += getValue(PlayCard[2])
+        print("Your cards are " + PlayCard[0] + " || " + PlayCard[1] + " || " + PlayCard[2])
+    elif res == 'D':
+        double()
     elif res == 'S':
         stand()     #call stand function
-    elif res == 'D':
-        double()    #call double function
+   #call double function
 
-    while()
+    if PlaySum > 21:
+        print("BUST!!!")
+        return False
+
+    while PlaySum<=21:
+        print("Do you want to HIT or STAND (H/S)")
+        res = input().upper()
+        if res == 'H':
+            hit()
+
+    if PlaySum > 21:
+        print("BUST!!!")
+        return False
+
+
 
 
 
 
 startRound()
+
+
+
+
+
+
 # main
 # money =  enter_money()
 #
